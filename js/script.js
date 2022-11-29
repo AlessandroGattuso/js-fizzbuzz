ba2cb67a426bed606c13e88c410f52250f99c8b1
Alessandro <alessandrogattuso@yahoo.it>
2022-11-29 15:20:06 +0100

    exersise done

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let container = document.getElementById('container');
+
+for(let i = 1; i <= 100; i++){
+  if(!(i%3) && !(i%5))
+    container.innerHTML +=  `<div class="box both-box">FizzBuzz</div>`;
+
+  else if(!(i%3))
+    container.innerHTML +=  `<div class="box fizz-box">Fizz</div>`;
+
+  else if(!(i%5))
+    container.innerHTML +=  `<div class="box buzz-box">Buzz</div>`;
+
+  else
+    container.innerHTML +=  `<div class="box">${i}</div>`;
+
+}
